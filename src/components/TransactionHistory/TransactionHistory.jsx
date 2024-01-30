8d5204725db0a4b1439801c189bd47dd8f2d4de3
Евгения Додиван <evgenita.d97@gmail.com>
2024-01-30 17:00:22 +0100

    update components

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,21 @@
+import { TransactionHistoryItem } from "../TransactionHistoryItem/TransactionHistoryItem"
+import css from "./TransactionHistory.module.css"
+
+export const TransactionHistory = ({ items }) => {
+    return (
+        <table className={css.transactionHistory}>
+            <thead >
+                <tr >
+                    <th className={css.title}>Type</th>
+                    <th className={css.title}>Amount</th>
+                    <th className={css.title}>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map(item => (
+                    <TransactionHistoryItem key={item.id} item={item} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
