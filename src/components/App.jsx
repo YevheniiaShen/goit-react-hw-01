683c0345e47b700cdc0b314683cb1f6e7385e86a
Евгения Додиван <evgenita.d97@gmail.com>
2024-01-30 16:51:06 +0100

    com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import userData from "../userData.json";
+import { Profile } from "./Profile/Profile";
 
-function App() {
-  const [count, setCount] = useState(0)
+import friends from "../friends.json"
+import { FriendList } from "./FriendList/FriendList";
 
+import transactions from "../transactions.json";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
+
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Profile userData={userData}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
-  )
-}
-
-export default App
+  );
+};
